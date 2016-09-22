53b5bb9ec99202a1f86b02bae21ee5bc5e821c66
Sergio Lozano Bueno <selobu@floridauniversitaria.es>
2016-09-22 10:11:31 +0200

    ejercicio3

---
+++ ejercicio3.php
@@ -0,0 +1,16 @@
+ <?php
+ ?>
+<html>
+  <head>
+      <title>ACTIVIDAD 03</title>
+  </head>
+  	<body>
+<?php
+		echo "Mi nombre es Sergio <br>";
+        echo "Mi primer apellido es Lozano <br>";
+        print "Mi segundo apellido es Bueno <br>";
+      ?>
+
+  	</body>
+</html>
+
